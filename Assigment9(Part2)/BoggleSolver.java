7b76f80c2882c0bf807feef6f5710959898f1b7c
irenkamalova <iren.kamalova@gmail.com>
2016-06-08 01:39:37 +0300

    87%

--- Assigment9(Part2)/BoggleSolver.java
+++ Assigment9(Part2)/BoggleSolver.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
-
-
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.TST;
 
 
 public class BoggleSolver {
@@ -12,7 +9,7 @@ public class BoggleSolver {
     private int N;
     private BoggleBoard board;
     private List<String> l = new ArrayList<>();
-    private TST<Integer> tst;
+    private MyTST<Integer> tst;
     
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
@@ -22,7 +19,7 @@ public class BoggleSolver {
         //for (int i = 0; i < dictionary.length; i++) {
         //    set.add(dictionary[i]);
         //}
-        tst = new TST<>();
+        tst = new MyTST<>();
         for (Integer i = 0; i < dictionary.length; i++) {
             
             tst.put(dictionary[i], i);
@@ -51,14 +48,15 @@ public class BoggleSolver {
         N = board.cols();
         boolean[][] marked = new boolean[M][N];
         l.clear();
+        for (int k = 0; k < M; k++) {
+            for (int m = 0; m < N; m++) {      
+                marked[k][m] = false;
+            }
+        }
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {                
                 StringBuilder path = new StringBuilder(board.getLetter(i, j));
-                for (int k = 0; k < M; k++) {
-                    for (int m = 0; m < N; m++) {      
-                        marked[k][m] = false;
-                    }
-                }
+                
                 searchingPaths(path, i, j, marked.clone());
             }
         }
@@ -85,13 +83,19 @@ public class BoggleSolver {
             //System.out.println(path);
             npath.append(path);
             npath.append(board.getLetter(i, j));
-            String s = new String(npath);
-            if (tst.keysWithPrefix(s).iterator().hasNext()) {
+            String s = npath.toString();
+            if (board.getLetter(i, j) == 'Q') {
+                //if (s.length() > 2 && tst.contains(s) && !l.contains(s)) {
+                //    l.add(s);
+                //}
+                npath.append('U');
+                s = npath.toString();
+            }
+            
+            if (tst.hasPrefix(s)) {
                 //tst.keysWithPrefix(s) != null || 
-                if (s.length() > 2 && tst.contains(s) && !l.contains(s))
+                if (s.length() > 2 && tst.contains(s) && !l.contains(s)) {
                     l.add(s);
-                if(board.getLetter(i, j) == 'Y') {
-                    //System.out.println('Y');
                 }
                 // go in 8 cases 
                 searchingPaths(npath, i - 1, j - 1, newmarked); //1
@@ -113,6 +117,68 @@ public class BoggleSolver {
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
+    private class MyTST<Value> {
+        private Node<Value> root;   // root of TST
+
+        private class Node<Value> {
+            private char c;                        // character
+            private Node<Value> left, mid, right;  // left, middle, and right subtries
+            private Value val;                     // value associated with string
+        }
+        
+        public MyTST() {
+        }
+        
+        public void put(String key, Value val) {
+            if (!contains(key)) N++;
+            root = put(root, key, val, 0);
+        }
+        
+        public boolean contains(String key) {
+            return get(key) != null;
+        }
+        
+        public Value get(String key) {
+            if (key == null) throw new NullPointerException();
+            if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+            Node<Value> x = get(root, key, 0);
+            if (x == null) return null;
+            return x.val;
+        }
+        
+        private Node<Value> put(Node<Value> x, String key, Value val, int d) {
+            char c = key.charAt(d);
+            if (x == null) {
+                x = new Node<Value>();
+                x.c = c;
+            }
+            if      (c < x.c)               x.left  = put(x.left,  key, val, d);
+            else if (c > x.c)               x.right = put(x.right, key, val, d);
+            else if (d < key.length() - 1)  x.mid   = put(x.mid,   key, val, d+1);
+            else                            x.val   = val;
+            return x;
+        }
+        
+        // return subtrie corresponding to given key
+        private Node<Value> get(Node<Value> x, String key, int d) {
+            if (key == null) throw new NullPointerException();
+            if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+            if (x == null) return null;
+            char c = key.charAt(d);
+            if      (c < x.c)              return get(x.left,  key, d);
+            else if (c > x.c)              return get(x.right, key, d);
+            else if (d < key.length() - 1) return get(x.mid,   key, d+1);
+            else                           return x;
+        }
+        
+        public boolean hasPrefix(String prefix) {
+            //Queue<String> queue = new Queue<String>();
+            Node<Value> x = get(root, prefix, 0);
+            if (x == null) return false;
+            else return true;
+        }
+    }
+    
     
     public int scoreOf(String word) {
         return 0;
@@ -122,8 +188,13 @@ public class BoggleSolver {
         In in = new In("dictionary-yawl.txt");
         String[] s = in.readAllStrings();
         
-        BoggleBoard board = new BoggleBoard("board-points1.txt");
+        BoggleBoard board = new BoggleBoard("board-quinquevalencies.txt");
         BoggleSolver bs = new BoggleSolver(s);
+        long startTime = System.currentTimeMillis();
+        bs.getAllValidWords(board);
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        System.out.println("Time is " + elapsedTime);
         for(String result : bs.getAllValidWords(board)) {
             System.out.println(result);
         }
